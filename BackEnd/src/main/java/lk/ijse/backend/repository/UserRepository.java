cc4a44bd05b923bbefe774cbe1d980cec4892e6f
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-08-22 01:28:23 +0530

    Jwt Util

--- BackEnd/src/main/java/lk/ijse/backend/repository/UserRepository.java
+++ BackEnd/src/main/java/lk/ijse/backend/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package lk.ijse.backend.repository;
 
 import lk.ijse.backend.entity.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository {
     Optional<User> findByUsername(String username);
 }
