0cc79eca0fe1f4b2d64e0a4d3d66d3f496abfc23
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-08-22 00:57:50 +0530

    Role Enum

---
+++ BackEnd/src/main/java/lk/ijse/backend/enums/Role.java
@@ -0,0 +1,19 @@
+package lk.ijse.backend.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum Role {
+    USER,
+    ADMIN;
+
+    @JsonCreator
+    public static Role fromString(String role) {
+        return valueOf(role.toUpperCase());
+    }
+
+    @JsonValue
+    public String toLowerCase() {
+        return this.name().toLowerCase();
+    }
+}
