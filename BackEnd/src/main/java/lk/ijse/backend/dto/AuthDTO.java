056b585e556fad74cac2e007270623baaeebd5fd
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-08-22 00:20:40 +0530

    :wrench: Update DTO

--- BackEnd/src/main/java/lk/ijse/backend/dto/AuthDTO.java
+++ BackEnd/src/main/java/lk/ijse/backend/dto/AuthDTO.java
@@ -1,5 +1,8 @@
 package lk.ijse.backend.dto;
 
+import lombok.Data;
+
+@Data
 public class AuthDTO {
     private String username;
     private String password;
--- BackEnd/src/main/java/lk/ijse/backend/dto/RegisterDTO.java
+++ BackEnd/src/main/java/lk/ijse/backend/dto/RegisterDTO.java
@@ -1,5 +1,8 @@
 package lk.ijse.backend.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterDTO {
     private String username;
     private String password;
