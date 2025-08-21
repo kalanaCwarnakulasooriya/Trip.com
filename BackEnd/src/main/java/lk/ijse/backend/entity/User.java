d65b9ec60da422384ca309d02d7f060d8c46d3a5
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-08-22 00:50:07 +0530

    User Entity

---
+++ BackEnd/src/main/java/lk/ijse/backend/entity/User.java
@@ -0,0 +1,26 @@
+package lk.ijse.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String role;
+}
