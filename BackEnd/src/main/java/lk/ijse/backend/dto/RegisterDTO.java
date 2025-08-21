0b889e0b432be443eaec66e2365d1a38a4f1721d
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-08-22 00:11:02 +0530

    Register DTO

---
+++ BackEnd/src/main/java/lk/ijse/backend/dto/RegisterDTO.java
@@ -0,0 +1,9 @@
+package lk.ijse.backend.dto;
+
+public class RegisterDTO {
+    private String username;
+    private String password;
+    private String email;
+    private int phone;
+    private String role;
+}
