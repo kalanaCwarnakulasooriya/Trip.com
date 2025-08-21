39f4e9e69eddc7427fc143d013a05546ddaa9979
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-08-22 02:45:18 +0530

    Security Config

---
+++ BackEnd/src/main/java/lk/ijse/backend/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package lk.ijse.backend.config;
+
+import lk.ijse.backend.util.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final UserDetailsService USERDETAILSSERVICE;
+    private final JwtAuthFilter JWTAUTHFILTER;
+    private final PasswordEncoder PASSWORDENCODER;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth-> auth
+                        .requestMatchers(
+                                "/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session-> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(JWTAUTHFILTER,
+                        UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider
+                = new DaoAuthenticationProvider();
+        daoAuthenticationProvider
+                .setUserDetailsService(USERDETAILSSERVICE);
+        daoAuthenticationProvider
+                .setPasswordEncoder(PASSWORDENCODER);
+        return daoAuthenticationProvider;
+
+    }
+}
