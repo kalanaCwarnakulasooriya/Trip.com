c955f2ba02d7a574f85fe0ed2aa5dd9dfb696c6c
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-08-27 20:57:28 +0530

    :wrench: Update Util

--- BackEnd/src/main/java/lk/ijse/backend/util/JwtAuthFilter.java
+++ BackEnd/src/main/java/lk/ijse/backend/util/JwtAuthFilter.java
@@ -1,6 +1,5 @@
 package lk.ijse.backend.util;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
--- BackEnd/src/main/java/lk/ijse/backend/util/JwtUtil.java
+++ BackEnd/src/main/java/lk/ijse/backend/util/JwtUtil.java
@@ -3,10 +3,8 @@ package lk.ijse.backend.util;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
 import java.util.Date;
 
 @Component
