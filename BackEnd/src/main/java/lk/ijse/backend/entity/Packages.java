cdbdd9c791bf0c890904f7c434b0e8a5f14694a2
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-09-03 01:18:01 +0530

    Travel Packages Entity

---
+++ BackEnd/src/main/java/lk/ijse/backend/entity/Packages.java
@@ -0,0 +1,36 @@
+package lk.ijse.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Packages {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tag;
+
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @ElementCollection
+    @CollectionTable(name = "package_features", joinColumns = @JoinColumn(name = "package_id"))
+    @Column(name = "feature")
+    private List<String> features;
+
+    private double price;
+
+    private String unit;
+}
