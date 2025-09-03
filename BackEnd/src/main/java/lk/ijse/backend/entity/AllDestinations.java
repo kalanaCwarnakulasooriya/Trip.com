3534dbfa856e2a92dd3f7b9c8831190afff98c88
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-09-03 16:15:00 +0530

    All Destination Entity

---
+++ BackEnd/src/main/java/lk/ijse/backend/entity/AllDestinations.java
@@ -0,0 +1,37 @@
+package lk.ijse.backend.entity;
+
+import jakarta.persistence.*;
+import lk.ijse.backend.entity.enums.Currency;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AllDestinations {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 100)
+    private String location;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private Double rating;
+
+    @Enumerated(EnumType.STRING)
+    private Currency currency;
+
+    @Column(nullable = false)
+    private Double price;
+}
