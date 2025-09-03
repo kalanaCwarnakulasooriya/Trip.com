ff46ceea9cc585a0185548c70b7fd0f6a3e0a5eb
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-09-03 12:07:52 +0530

    Contact Entity

---
+++ BackEnd/src/main/java/lk/ijse/backend/entity/Contact.java
@@ -0,0 +1,30 @@
+package lk.ijse.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "subject", nullable = false)
+    private String subject;
+
+    @Column(name = "message", columnDefinition = "TEXT", nullable = false)
+    private String message;
+}
