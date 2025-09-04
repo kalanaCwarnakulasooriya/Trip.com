62337ff1db118f49f0a172a743c2cc3e8d595f9b
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-09-04 23:51:45 +0530

    FeedBack DTO

---
+++ BackEnd/src/main/java/lk/ijse/backend/dto/FeedBackDTO.java
@@ -0,0 +1,15 @@
+package lk.ijse.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeedBackDTO {
+    private Long id;
+    private String name;
+    private String trip;
+    private String message;
+}
