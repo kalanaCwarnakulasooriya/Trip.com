25f1ca917883ae8387c2f8a3180ecf80d284b999
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-09-04 23:53:23 +0530

    FeedBack Entity

---
+++ BackEnd/src/main/java/lk/ijse/backend/entity/FeedBack.java
@@ -0,0 +1,27 @@
+package lk.ijse.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FeedBack {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String trip;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String message;
+}
