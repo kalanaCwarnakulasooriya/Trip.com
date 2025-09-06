3764f35709d12a4a038d2c644eea042b6f40647b
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-09-07 00:06:32 +0530

    Packages Table loaded

--- BackEnd/src/main/java/lk/ijse/backend/dto/PackagesDTO.java
+++ BackEnd/src/main/java/lk/ijse/backend/dto/PackagesDTO.java
@@ -10,6 +10,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PackagesDTO {
+    private Long id;
     private String tag; // Most Popular,Best Value,New
     private String title;
     private String description;
