3627e3b4dcb7d6aa08a4666c1c45ce6c70753683
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-09-07 00:36:59 +0530

    Contact Table loaded

--- BackEnd/src/main/java/lk/ijse/backend/dto/ContactDTO.java
+++ BackEnd/src/main/java/lk/ijse/backend/dto/ContactDTO.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ContactDTO {
+    private Long id;
     private String name;
     private String email;
     private String subject;
