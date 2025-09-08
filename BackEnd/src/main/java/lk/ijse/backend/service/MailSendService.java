819bd2ac8fe58b39ad73981df84675ca53c0f56e
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-09-08 21:48:46 +0530

    Spring Mail Service

---
+++ BackEnd/src/main/java/lk/ijse/backend/service/MailSendService.java
@@ -0,0 +1,6 @@
+package lk.ijse.backend.service;
+
+public interface MailSendService {
+    void sendRegisteredEmail(String name, String email, String subject);
+    void sendLoggedInEmail(String userName, String toEmail, String subject);
+}
