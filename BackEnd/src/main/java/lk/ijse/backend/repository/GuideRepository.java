59b0d48fd6e4d0b88d0638e70d90614b105a17be
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-09-09 22:22:23 +0530

    Guide Repository

---
+++ BackEnd/src/main/java/lk/ijse/backend/repository/GuideRepository.java
@@ -0,0 +1,9 @@
+package lk.ijse.backend.repository;
+
+import lk.ijse.backend.entity.Guide;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuideRepository extends JpaRepository<Guide, Long> {
+}
