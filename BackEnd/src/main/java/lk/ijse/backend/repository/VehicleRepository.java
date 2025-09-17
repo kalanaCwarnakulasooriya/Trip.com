8bb2903390ffd96ffa962206011386737a77c3cf
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-09-17 15:09:57 +0530

    VehicleRepository

---
+++ BackEnd/src/main/java/lk/ijse/backend/repository/VehicleRepository.java
@@ -0,0 +1,9 @@
+package lk.ijse.backend.repository;
+
+import lk.ijse.backend.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+}
