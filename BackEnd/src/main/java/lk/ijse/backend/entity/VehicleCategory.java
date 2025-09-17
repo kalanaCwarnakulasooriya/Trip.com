c91b110706a0bae35f42988ef3f675fbff210fff
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-09-17 23:28:37 +0530

    Vehicle Category Entity

---
+++ BackEnd/src/main/java/lk/ijse/backend/entity/VehicleCategory.java
@@ -0,0 +1,36 @@
+package lk.ijse.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VehicleCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(columnDefinition = "TEXT")
+    private String iconSvg;
+
+    @Column(nullable = false)
+    private double pricePerDay;
+
+    @Column(nullable = false)
+    private Integer availableVehicles;
+
+    @Column(length = 200)
+    private String buttonLink;
+}
