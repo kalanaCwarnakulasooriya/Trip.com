0c5a846f73711fe1ec83a5719f07d2aa8c19dad6
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-09-18 12:26:53 +0530

    RentalProcessStep Entity

---
+++ BackEnd/src/main/java/lk/ijse/backend/entity/RentalProcessStep.java
@@ -0,0 +1,30 @@
+package lk.ijse.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RentalProcessStep {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private String iconSvg;
+
+    @Column(nullable = false)
+    private Integer stepNumber;
+}
